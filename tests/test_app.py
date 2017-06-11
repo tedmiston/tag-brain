b375bed56f9677ed6d4256df4406f9b5cbd24682
Taylor Edmiston <tedmiston@gmail.com>
2017-06-10 23:19:22 -0400

    Add unit tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,7 +2,7 @@ from unittest.mock import mock_open, patch
 
 from nose2.tools.params import params
 
-from app import de_dupe, de_hash, load_input
+from app import build_header, create_tag_str, de_dupe, de_hash, get_collapser, load_input
 
 
 def check_equalish(list1, list2):
@@ -49,3 +49,36 @@ def test_load_input(input_file_contents, caption, tags_out):
     m = mock_open(read_data=input_file_contents)
     with patch('builtins.open', m):
         assert load_input('_.txt') == (caption, tags_out)
+
+
+@params(
+    ([], '0 tags'),
+    (['a'], '1 tags'),
+    (list('ab'), '2 tags'),
+    (list('abc'), '3 tags'),
+    (list('abcdefghijklmnopqrstuvwxyz012'), '29 tags'),
+    (list('abcdefghijklmnopqrstuvwxyz0123'), '30 tags'),
+    (list('abcdefghijklmnopqrstuvwxyz01234'), '31 tags *Warning: You have exceeded the max of 30 tags*'),
+)
+def test_build_header(tags, expected):
+    assert build_header(tags=tags) == expected
+
+
+@params(
+    (True, '\n.\n.\n.\n.\n.'),
+    (False, ''),
+)
+def test_get_collapser(show, expected):
+    assert get_collapser(show=show) == expected
+
+
+@params(
+    ([], True, ''),
+    ([], False, ''),
+    (['a'], True, '#a'),
+    (['a'], False, '#a'),
+    (['a', 'b', 'c'], True, '#a #b #c'),
+    (['a', 'b', 'c'], False, '#a\n#b\n#c'),
+)
+def test_create_tag_str(tags, spaces, expected):
+    assert create_tag_str(tags=tags, spaces=spaces) == expected
